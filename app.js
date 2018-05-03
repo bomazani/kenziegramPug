2c325740cb6f10eac9a6ddb7774139aabbd4f7cf
Bob Hunt <bob.hunt.email@gmail.com>
2018-05-03 15:37:21 -0400

    successfully applied pug to kenziegram

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const upload = multer({ dest: 'public/uploads' });
+// set port to localhost:3000
 const port = 3000;
 const app = express();
 
@@ -11,43 +12,20 @@ const uploadsPath = publicPath + '/uploads/';
 
 const uploaded_files = [];
 
+app.set('view engine','pug');
+// app.use(express.static(./public))
 app.use(express.static(publicPath));
+// app.use(express.static(./public/uploads/))
 app.use(express.static(uploadsPath));
 
-const html = {
-    head: `
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <meta charset="utf-8">
-            <title>My Kenziegram</title>
-            <link rel="stylesheet" href="./style.css">
-        </head>
-    `,
-    bodyHeader: `
-        <body>
-            <h1>Welcome to Kenziegram!</h1>
-    `,
-    form: `
-        <form action="http://localhost:3000/uploads" method="post" enctype="multipart/form-data">
-            <div>
-            <label for="file">Choose a File</label>
-            <!-- <input type="file" id="file" name="myFile"> -->
-            <input type="file" id="file" name="myFile">
-            
-            <!-- <input type="file" name="file" id="file" accept="image/*" multiple> -->
-            </div>
-            <div>
-            <button>Send the file</button>
-            </div>
-        </form>
-    `,
-    foot: `
-        <script src="./index.js"></script>
-        </body>
-        </html>
-    `,
-}
+// const html = {
+//     `,
+//     foot: `
+//         <script src="./index.js"></script>
+//         </body>
+//         </html>
+//     `,
+// }
 
 
 
@@ -55,31 +33,21 @@ app.get('/', (req, res) => {
     fs.readdir(uploadsPath, function (err, items) {
         console.log(items);
         
-        let htmlImageGallery = ``;
-        for (let i = 0; i < items.length; i++) {
-            htmlImageGallery += `<img src="${ items[i] }">`;
-        }
-
-        const htmlOutput = 
-            html.head + 
-            html.bodyHeader + 
-            html.form + 
-            htmlImageGallery + 
-            html.foot;
 
         console.log("code that will run in node, instead of in the browser")
-        res.send(htmlOutput);
+        res.render('index', {items})
     });
 });
 
-app.post('/uploads', upload.single('myFile'), function (req, res, next) {
 
-    // req.file is the `myFile` file
-    // req.body will hold the text fields, if there are any
+app.post('/uploads', upload.single('myFile'), function (req, res, next) {
+    
+    let html = '<a href="/">Return</a>';
+
     console.log("Uploaded: " + req.file.filename);
     // console.log(`Uploaded: ${req.file.filename}`;    
     uploaded_files.push(req.file.filename);
-    res.end("Uploaded file!");
+    res.render('success');
 });
 
 
